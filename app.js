7e92a1d1ba8879d8adfeebeb7c15897be4c6b035
padrisimo <nopuedoparardemolar@outlook.com>
2017-11-18 21:35:12 +0100

    templating in ejs

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ var path = require('path');
 
 var app = express();
 
-/* var logger = function(req, res, next){
-    console.log('Loggin...');
-    next();
-}
-
-app.use(logger); */
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,8 +14,32 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Set static Path
 app.use(express.static(path.join(__dirname, 'public')))
 
+var users = [
+    {
+        id:1,
+        first_name: 'Manolo',
+        last_name: 'Escobar',
+        email: 'escobar@gmail.com'
+    },
+    {
+        id:2,
+        first_name: 'Pedro',
+        last_name: 'Picapiedra',
+        email: 'rocadura@gmail.com'
+    },
+    {
+        id:1,
+        first_name: 'Lola',
+        last_name: 'Flores',
+        email: 'simequereisirsen@gmail.com'
+    }
+]
+
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', {
+        title: 'Passengers',
+        users: users
+    });
 })
 
 app.listen(3000, function(){
